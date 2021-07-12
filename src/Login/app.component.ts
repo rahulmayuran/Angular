37ebf75dd1672e729f715b769bb68688fa9d386b
rahulmayuran <rahulmayuran1996@gmail.com>
2021-07-12 11:06:54 +0530

    added css effects

--- src/Login/app.component.ts
+++ src/Login/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Integration';
+  title = 'Flight App';
+  flightImg:string = "assets/flightImage.jpg"
 }
