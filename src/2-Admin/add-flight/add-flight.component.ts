35ce5c20090540511dabb088e9ae2ed2d1d21ac9
rahulmayuran <rahulmayuran1996@gmail.com>
2021-07-19 22:18:37 +0530

    Added bootstrap effects

--- src/2-Admin/add-flight/add-flight.component.ts
+++ src/2-Admin/add-flight/add-flight.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FlightService } from 'src/3-Services/flight.service';
 
 @Component({
   selector: 'app-add-flight',
@@ -8,9 +11,44 @@ import { Component } from '@angular/core';
 
 export class AddFlightComponent{
 
+  flight:any = {};
+  message:string=''
+  flightForm:FormGroup;
 
-  constructor() 
-  { }
+  constructor(private flightRouter:Router, private fservice:FlightService) 
+  { 
+    this.flightForm = new FormGroup({
+      AirlineName : new FormControl("",Validators.required),
+      model : new FormControl("",Validators.required),
+      contact : new FormControl("",Validators.required)
+    })
+  }
+
+  
+  addFlight()
+  {
+    if(this.checkFlight())
+    return;
+    this.fservice.getFlightByName(this.flight.name).subscribe((data)=>{
+      if(data[0]!=null){
+      }else{
+        this.fservice.saveFlight(this.flight);
+        this.flightRouter.navigateByUrl('/manageFlights');
+      }
+   
+  });
+}
+
+  checkFlight():boolean
+  {
+    if(this.flight.addAirline =='' || this.flight.contact==''
+        || this.flight.model=='')
+        {
+     this.message = "Kindly fill all the details"
+      return true;
+    }
+    return false;
+  }
 
 }
   
