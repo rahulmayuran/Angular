3ec4aa82e406a6c1f8308977781e043d4998dcea
rahulmayuran <rahulmayuran1996@gmail.com>
2022-01-22 17:03:23 +0530

    Made changes to Stock adding

---
+++ src/2-Admin/add-company/add-company.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-flight',
+  templateUrl: './add-flight.component.html',
+  styleUrls:['./add-flight.component.css']
+})
+
+export class AddFlightComponent implements OnInit{
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
